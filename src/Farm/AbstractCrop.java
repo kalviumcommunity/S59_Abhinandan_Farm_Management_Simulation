07e5f978f26c4392b0b832eb61ee893caab0d387
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-08-26 15:38:01 +0530

    used abstraction

---
+++ src/Farm/AbstractCrop.java
@@ -0,0 +1,40 @@
+package Farm;
+
+public abstract class AbstractCrop {
+    protected String type;
+    protected int waterLevel;
+    protected String description;
+
+    public AbstractCrop(String type, int initialWaterLevel, String description) {
+        this.type = type;
+        this.waterLevel = initialWaterLevel;
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(int waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract void waterCrop(int amount);
+    public abstract void displayInfo();
+}
