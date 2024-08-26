24b25d447e66665617a710ab2aef09eded74f6c9
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-08-26 13:19:46 +0530

    encapsulation done

---
+++ src/Farm/Farmer.java
@@ -0,0 +1,42 @@
+// use of encapsulation
+
+package Farm;
+
+public class Farmer {
+    private String name;
+    private int age;
+    private static int farmerCount;
+
+    public Farmer(String name, int age) {
+        this.name = name;
+        this.age = age;
+        farmerCount++;
+    }
+
+    public static int getFarmerCount() {
+        return farmerCount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age =  age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void displayFarmerInfo() {
+        System.out.println("Farmer Information: ");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+};
